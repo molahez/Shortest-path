6fa2fe2149943774f8db8359e1ce547f1336381e
abdelrahman <abdelrahman.alsayed.ahmed@gmail.com>
2019-05-17 01:28:10 +0200

    Done

--- src/eg/edu/alexu/csd/filestructure/graphs/test/Graph.java
+++ src/eg/edu/alexu/csd/filestructure/graphs/test/Graph.java
@@ -19,6 +19,10 @@ public class Graph implements IGraph {
 	ArrayList<Integer> neighbours = new ArrayList<Integer>();
 	double infinity = Double.POSITIVE_INFINITY;
 	Graphe graphe = null;
+	int[][] graph;
+	int numOfVertices = 0;
+	ArrayList<Integer> verticesOrdered = new ArrayList<Integer>();
+	int[] dist;
 
 	class Graphe {
 		// A class to represent a weighted edge in graph
@@ -60,12 +64,19 @@ public class Graph implements IGraph {
 					l1 = line.split(" ");
 					graphe = new Graphe(Integer.parseInt(l1[0]), Integer.parseInt(l1[1]));
 					linecounter++;
+
+					// For Dijkstra
+					graph = new int [Integer.parseInt(l1[0])][Integer.parseInt(l1[0])];
+					numOfVertices = Integer.parseInt(l1[0]);
 				} else {
 					String[] l2 = line.split(" ");
 					graphe.edge[linecounter - 1].src = Integer.parseInt(l2[0]);
 					graphe.edge[linecounter - 1].dest = Integer.parseInt(l2[1]);
 					graphe.edge[linecounter - 1].weight = Integer.parseInt(l2[2]);
 					linecounter++;
+
+					// For Dijkstra
+					graph[Integer.parseInt(l2[0])][Integer.parseInt(l2[1])] = Integer.parseInt(l2[2]);
 
 				}
 			}
@@ -112,14 +123,53 @@ public class Graph implements IGraph {
 
 	@Override
 	public void runDijkstra(int src, int[] distances) {
-		// TODO Auto-generated method stub
+		Boolean visitedArr[] = new Boolean[numOfVertices];
+
+		for (int i = 0; i < numOfVertices; i++)
+        {
+			distances[i] = (Integer.MAX_VALUE)/2;
+            visitedArr[i] = false;
+        }
+
+		distances[src] = 0;
+
+		for (int count = 0; count < numOfVertices -  1; count++)
+        {
+
+            int u = getMinAfterItr(distances, visitedArr);
+
+
+            visitedArr[u] = true;
+
+            for (int v = 0; v < numOfVertices; v++)
+
+
+                if (!visitedArr[v] && graph[u][v]!=0 &&
+                		distances[u] != (Integer.MAX_VALUE)/2 &&
+                	    distances[u]+graph[u][v] < distances[v])
+                	distances[v] = distances[u] + graph[u][v];
+        }
+		dist = new int[numOfVertices];
+		dist = distances;
 
 	}
 
 	@Override
 	public ArrayList<Integer> getDijkstraProcessedOrder() {
-		// TODO Auto-generated method stub
-		return null;
+		   int min = dist[0];
+
+	       for (int j = 0; j < numOfVertices; j ++) {
+	    	   for(int i = 0; i < dist.length; i++)
+		       {
+		            if(min > dist[i])
+		            {
+		                min = dist[i];
+		                verticesOrdered.add(i);
+		                dist[i] = (Integer.MAX_VALUE)/2;
+		            }
+		        }
+	       }
+	       return verticesOrdered;
 	}
 
 	@Override
@@ -158,5 +208,20 @@ public class Graph implements IGraph {
 
 		}
 	}
+
+	private int getMinAfterItr (int dist[], Boolean visitedArr[])
+    {
+        // Initialize minimum value
+        int min = (Integer.MAX_VALUE)/2 , min_index=-1;
+
+        for (int v = 0; v < numOfVertices; v++)
+            if (visitedArr[v] == false && dist[v] <= min)
+            {
+                min = dist[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
 
 }
